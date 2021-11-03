3ef9c2256d4011d2b6bbfe37167ed0d5d8b39e1d
donaldte <donaldtedom0@gmail.com>
2021-11-03 07:24:51 +0100

    admin change

--- afrikanImange/urls.py
+++ afrikanImange/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('aviorre/', admin.site.urls),
     path('', include('images.urls'))
 ]
 
